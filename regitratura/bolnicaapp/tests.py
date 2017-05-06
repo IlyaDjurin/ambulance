d4bdfe3714b04ea8dcc1f6ed078cfc15cb364f6f
IlyaDjurin <lev2048@gmail.com>
2017-05-06 09:00:08 +0300

    test

--- regitratura/bolnicaapp/tests.py
+++ regitratura/bolnicaapp/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from bolnicaapp.models import Doctor, Reception
+from datetime import datetime, date, time
+
+
+
+# Create your tests here.
+
+
+
+class DoctorModelTest(TestCase):
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Doctor._meta.verbose_name_plural), "Врачи")
+
+    def test_verbose_name(self):
+        self.assertEqual(str(Doctor._meta.verbose_name), "Врач")
+
+    def test_doc_database(self):
+        Doctor.objects.create(name="Тимченко Сергей", specialization="Стоматолог", info="Лечит зубы")
+        doc = Doctor.objects.get(specialization="Стоматолог")
+        self.assertEqual(str(doc.name), "Тимченко Сергей")
+
+
+class ReceptionModelTest(TestCase):
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Reception._meta.verbose_name_plural), "Приемы")
+
+    def test_verbose_name(self):
+        self.assertEqual(str(Reception._meta.verbose_name), "Прием")
+
+    def test_reception_database(self):
+        d = Doctor.objects.create(name="Тимченко Сергей", specialization="Стоматолог", info="Лечит зубы")
+
+        Reception.objects.create(date=date(2017, 5, 6), time=time(9, 00),
+                                 patient_name="Вася",
+                                 patient_info="У Васи болит зуб",
+                                 doctor=d)
+        rec = Reception.objects.get(id=1)
+        self.assertEqual(str(rec.patient_name), "Вася")
+
+
+class ViewsTest(TestCase):
+    def test_call_view_loads(self):
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_call_view_admin(self):
+
+        response = self.client.get('/http://127.0.0.1:8000/admin/')
+
+        self.assertEqual(response.status_code, 200)
+
+
--- regitratura/bolnicaapp/views.py
+++ regitratura/bolnicaapp/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import FormView
 from .models import Doctor, Reception
 from .forms import ReceptionForm
-from django.http import JsonResponse
+from datetime import datetime, date, time
 from django.template import RequestContext
 
 class DoctorList(ListView):
@@ -29,8 +29,17 @@ class ReceptionView(FormView):
                        "doctor":curr_doctor,
                        "curr_date":fcd['date'],
                        "curr_time":fcd['time']}
-        # условие для предотвращения записи на один и тот же деь у данного врача
-        if Reception.objects.filter(date=fcd['date'],time=fcd['time'],doctor=curr_doctor).count()==0:
+        d = fcd['date']
+        d1 = datetime.isoweekday(d)
+        print(d1)
+        t = fcd['time']
+        t1 = time(9, 00)
+        t2 = time(17, 00)
+        # условие для предотвращения записи на один и тот же день,на то же время
+        #  или выходной день у данного врача
+        if Reception.objects.filter(date=fcd['date'],time=fcd['time'],doctor=curr_doctor).count()==0 and 1<=d1<=5 and t1<=t<=t2:
+
+
             Reception.objects.create(date=fcd['date'],time=fcd['time'],
                                      patient_name=fcd['patient_name'],
                                      patient_info=fcd['patient_info'],
@@ -38,7 +47,8 @@ class ReceptionView(FormView):
             response_dict["norm"] = "Вы записаны на прием"
             return render_to_response('reception.html', response_dict,)
         else:
-            response_dict["message"]="Это время уже занято, пожалуйста выберите другое время или дату"
+            response_dict["message"]="Это время уже занято,или вы пытаетесь записаться в не рабочее время или выходной день." \
+                                     " Пожалуйста выберите другое время или дату"
             return render_to_response('reception.html',response_dict,
                           )
 
@@ -46,13 +56,3 @@ class ReceptionView(FormView):
         context = super(ReceptionView, self).get_context_data(**kwargs)
         context['doctor']=Doctor.objects.get(id= self.kwargs['doctor_id'])
         return context
-
-
-def date_from_ajax (request):
-    if request.method == "POST" and request.is_ajax():
-        doctor=Doctor.objects.get(id= request.POST.get("doctor_id"))
-        reception_set=doctor.reception_set.filter(date=request.POST.get("date_from_ajax"))
-        time_list=[]
-        for reception in reception_set:
-            time_list.append(reception.time)
-        return JsonResponse({'time_list':time_list})
